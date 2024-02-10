c22d59bc1733814395268b3baa180f86b96867ab
Kevin Ulliac <kevanoullio@outlook.com>
2024-02-10 13:45:50 -0700

    Simplified Route code, will define functions in Controller and Repository.

--- controllers/monster.controller.js
+++ controllers/monster.controller.js
@@ -1,5 +1,15 @@
 import { getMonstersFromRepository } from "../repositories/monster.repository.js";
 
+
+
+// Need to implement the following:
+// get/ => return all monsters (array of objects)
+// get/id => return 1 specific monster (an object)
+// patch/id => return 200 (return the object) or return 202 (return nothing)
+// post/ => return 200 and the new object
+// delete/id => return 204 (return nothing)
+
+
 export const getMonsters = async (req, res) => {
     try {
         const monsters = await getMonstersFromRepository();
--- routes/monster.route.js
+++ routes/monster.route.js
@@ -1,41 +1,15 @@
 // Create the express router to handle our products requests
 import express from "express";
+import { getMonsters, getMonster, createMonster, updateMonster, deleteMonster } from "../controllers/monster.controller.js";
 
+// Create the express router to handle the monster requests
 const router = express.Router();
 
-
-// Need to implement the following:
-// get/ => return all monsters (array of objects)
-// get/id => return 1 specific monster (an object)
-// patch/id => return 200 (return the object) or return 202 (return nothing)
-// post/ => return 200 and the new object
-// delete/id => return 204 (return nothing)
-
-
 // Remove or rewrite everything below
-router.get("/", function (req, res) {
-    try {
-        
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-    res.send("On this call we show a list of products");
-});
-
-router.get("/:id", function (req, res) {
-    res.send(`On this call we show a product of ${req.params.id}`);
-});
-
-router.post("/", function (req, res) {
-    res.send(`On this call we create a product`);
-});
-
-router.put("/:id", function (req, res) {
-    res.send("On this call we update a product");
-});
-
-router.delete("/:id", function (req, res) {
-    res.send("On this call we delete a product");
-});
+router.get("/", getMonsters);
+router.get("/:id", getMonster);
+router.post("/", createMonster);
+router.patch("/:id", updateMonster);
+router.delete("/:id", deleteMonster);
 
 export default router;
